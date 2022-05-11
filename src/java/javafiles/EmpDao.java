c81ca20a51230924ac2e8ad004c025bcc6de2502
sparhawk2020 <cjjc20001@yahoo.com>
2022-05-11 13:10:36 -0700

    Ready to deploy

--- src/java/javafiles/EmpDao.java
+++ src/java/javafiles/EmpDao.java
@@ -1,5 +1,6 @@
 package javafiles;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
